34a07e911498d41e87c0c3fd60f70c9a489828f4
vaskivstanislav <stasmegatyr@gmail.com>
2025-02-11 16:06:15 +0200

    second check

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,62 +1,32 @@
-import { useSearchParams } from "react-router-dom";
-import { useEffect, useState } from "react";
-import toast, { Toaster } from "react-hot-toast";
-import MovieList from "../../components/MovieList/MovieList";
-import SearchBox from "../../components/SearchBox/SearchBox";
-import { getFilmsSearch } from "../../js/films-api"; // Актуальний API для пошуку фільмів
-import styles from "./MoviesPage.module.css";
+import { Link, useLocation } from "react-router-dom";
+import style from "../NotFoundPage/NotFoundPage.module.css";
+import notFoundPage from "../../assets/img/not-found-page.png";
 
-const MoviesPage = () => {
-  const [searchResults, setSearchResults] = useState([]);
-  const [searchParams, setSearchParams] = useSearchParams();
-  const searchQuery = searchParams.get("search");
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      setLoading(true);
-      try {
-        const query = searchQuery || "popular"; // Якщо немає запиту, шукаємо популярні фільми
-        const response = await getFilmsSearch(query);
-        
-        setSearchResults(response.results);
-        if (response.total_results === 0) {
-          toast.error("No results found.");
-        } else {
-          toast.success(`Found ${response.total_results} films.`);
-        }
-      } catch (error) {
-        console.error("Error fetching films:", error);
-        toast.error("Something went wrong, please try again.");
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchData();
-  }, [searchQuery]); 
-  const handleSearchSubmit = (query) => {
-    setSearchParams({ search: query }); 
-  };
+const NotFoundPage = () => {
+  const location = useLocation();
+  const backLinkHref = location.state?.from ?? "/";
 
   return (
-    <main>
-      <section className={styles.moviesSearch}>
-        <SearchBox onSubmit={handleSearchSubmit} />
-        <Toaster position="top-right" reverseOrder={false} />
+    <section className={style.containerNotFound}>
+      <div className={style.notFound}>
+        <h1 className={style.notFoundTitle}>404</h1>
 
-        {loading ? (
-          <div className={styles.loadingContainer}>
-            <p>Loading...</p>
-          </div>
-        ) : searchResults.length > 0 ? (
-          <MovieList filmsList={searchResults} />
-        ) : (
-          <p>No results found.</p>
-        )}
-      </section>
-    </main>
+        <img
+          className={style.notFoundImg}
+          src={notFoundPage}
+          alt="not found page"
+        />
+        <h2 className={style.notFoundTitleInform}>Page not found</h2>
+
+        <p className={style.notFoundMessage}>
+          We`re sorry, the page you requesterd could not be found
+        </p>
+        <Link to={backLinkHref}>
+          <button className={style.notFoundBtn}>Go to homepage</button>
+        </Link>
+      </div>
+    </section>
   );
 };
 
-export default MoviesPage;
+export default NotFoundPage;
