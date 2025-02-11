06430b340af4bc3afb10106ddea81cbdccafe5cc
vaskivstanislav <stasmegatyr@gmail.com>
2025-02-11 15:52:08 +0200

    first check

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,26 +1,15 @@
-import PropTypes from 'prop-types';
-import { MutatingDots } from 'react-loader-spinner';
-import styles from './Loader.module.css';
-import React from 'react';
+import { Puff } from "react-loader-spinner"; 
+import styles from "./Loader.module.css"; 
 
-const Loader = React.memo(({ size = 100, color = '#3498db' }) => {
-  return (
-    <div className={styles.loaderContainer} aria-label="Loading...">
-      <MutatingDots
-        height={size}
-        width={size}
-        color={color}
-        secondaryColor={color}
-        radius={size / 8}
-        ariaLabel="loading"
-      />
-    </div>
-  );
-});
-
-Loader.propTypes = {
-  size: PropTypes.number,
-  color: PropTypes.string,
-};
+const Loader = () => (
+  <div className={styles.loaderContainer}>
+    <Puff
+      height={100} 
+      width={100}  
+      color="#4fa94d" 
+      ariaLabel="puff-loading" 
+    />
+  </div>
+);
 
 export default Loader;
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import Modal from 'react-modal';
+import React from "react";
+import * as ReactDOM from "react-dom/client";
+import { HashRouter } from "react-router-dom";
+import App from "./components/App/App";
+import "./index.css";
 
-Modal.setAppElement('#root');
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <HashRouter basename="/">
+      <App />
+    </HashRouter>
+  </React.StrictMode>
+);
